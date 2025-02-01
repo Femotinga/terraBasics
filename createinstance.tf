d009257240820185ef07987014e0aaf1579b23b7
Femotinga <femiadeliyi@gmail.com>
2025-02-01 08:13:06 -0500

    added provider.tf

--- createinstance.tf
+++ createinstance.tf
@@ -1,8 +1,3 @@
-provider "aws" {
-    access_key = "AKIATBRPQONN3NXFRY6H"
-    secret_key = ""
-    region     = "eu-west-2"
-}
 
 resource "aws_instance" "myFirstInstance" {
     ami       = "ami-09a2a0f7d2db8baca"
