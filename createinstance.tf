122a043bb6c59005bb1dd7f542fcf7d9b5ffb416
Femotinga <femiadeliyi@gmail.com>
2025-02-01 10:00:42 -0500

    added more resources

--- createinstance.tf
+++ createinstance.tf
@@ -1,4 +1,9 @@
 resource "aws_instance" "myFirstInstance" {
+    count     = 3
     ami       = "ami-09a2a0f7d2db8baca"
     instance_type = "t2.micro"
+
+    tags{
+        Name = "threedemoinstances"
+    }
 }
